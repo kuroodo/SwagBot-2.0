c8d7736cdbda33bfde36a942d3cc09db707e45f2
Kuroodo <kuroododitory@gmail.com>
2019-09-18 19:34:06 -0400

    Update bot activity message

--- src/kuroodo/swagbot/config/BotConfig.java
+++ src/kuroodo/swagbot/config/BotConfig.java
@@ -6,7 +6,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class BotConfig {
 	public static final String BOTNAME = "SwagBot";
 	public static final String BOTVERSION = "2.0.0";
-	public static final String DEFAULT_GAMEMESSAGE = "Enter !help for help";
+	public static final String DEFAULT_GAMEMESSAGE = "@me help for help";
 	private JDA jda;
 
 	public BotConfig(JDA jda) {
