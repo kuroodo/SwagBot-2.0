0cd6d400c05a80102fbeac16dac62d72053820dc
Kuroodo <kuroododitory@gmail.com>
2019-09-18 15:51:21 -0400

    Add more utility methods to BotUtility

--- src/kuroodo/swagbot/utils/BotUtility.java
+++ src/kuroodo/swagbot/utils/BotUtility.java
@@ -1,11 +1,25 @@
 package kuroodo.swagbot.utils;
 
+import kuroodo.swagbot.guild.GuildManager;
+
 public class BotUtility {
 	public static String[] splitString(String string) {
 		return string.split("\\s+");
 	}
-	
+
 	public static String removePrefix(String prefix, String message) {
 		return message.substring(prefix.length());
+	}
+
+	public static String getGuildPrefix(long guildID) {
+		return GuildManager.getGuild(guildID).commandPrefix;
+	}
+	
+	public static String codifyText(String message) {
+		return "```css\n" + message + "\n```";
+	}
+	
+	public static String quotifyText(String message) {
+		return "> " + message;
 	}
 }
