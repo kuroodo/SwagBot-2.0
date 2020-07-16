4da75906fecabcdcd1beebfd924f9339dc5194a4
Kuroodo <kuroododitory@gmail.com>
2020-07-16 14:46:10 -0400

    Add null check for JsonObject when trying to read guild settings file and rewrite file if any keys had issues
    * This means that if a new setting is added and the file doesn't have it, it will write the missing key into the file

--- src/main/java/kuroodo/swagbot/json/GuildSettingsReader.java
+++ src/main/java/kuroodo/swagbot/json/GuildSettingsReader.java
@@ -37,39 +37,127 @@ public class GuildSettingsReader {
 	public static GuildSettings loadSettingsFile(long guildID) {
 		Reader reader;
 		GuildSettings settings = new GuildSettings(guildID);
+		boolean needsWriting = false;
 		try {
 			reader = new BufferedReader(new FileReader(JSONKeys.SETTINGS_PATH + guildID + ".json"));
-			JsonObject object = Json.parse(reader).asObject();
-
+			JsonObject jsObject = Json.parse(reader).asObject();
 			settings = new GuildSettings(guildID);
+
 			// booleans
-			settings.enableWelcome = Boolean.parseBoolean(object.get(JSONKeys.SETTINGS_ENABLE_WELCOME).asString());
-			settings.enableWelcomeRole = Boolean
-					.parseBoolean(object.get(JSONKeys.SETTINGS_ENABLE_WELCOME_ROLE).asString());
-			settings.spartankick = Boolean.parseBoolean(object.get(JSONKeys.SETTINGS_SPARTANKICK).asString());
+			if (!isObjectNull(JSONKeys.SETTINGS_ENABLE_WELCOME, jsObject)) {
+				settings.enableWelcome = getBool(JSONKeys.SETTINGS_ENABLE_WELCOME, jsObject);
+			} else {
+				needsWriting = true;
+			}
+
+			if (!isObjectNull(JSONKeys.SETTINGS_ENABLE_WELCOME_ROLE, jsObject)) {
+				settings.enableWelcomeRole = getBool(JSONKeys.SETTINGS_ENABLE_WELCOME_ROLE, jsObject);
+			} else {
+				needsWriting = true;
+			}
+
+			if (!isObjectNull(JSONKeys.SETTINGS_SPARTANKICK, jsObject)) {
+				settings.spartankick = getBool(JSONKeys.SETTINGS_SPARTANKICK, jsObject);
+			} else {
+				needsWriting = true;
+			}
 
 			// Strings
-			settings.commandPrefix = object.get(JSONKeys.SETTINGS_COMMAND_PREFIX).asString();
-			settings.welcomeMessage = object.get(JSONKeys.SETTINGS_WELCOME_MESSAGE).asString();
+			if (!isObjectNull(JSONKeys.SETTINGS_COMMAND_PREFIX, jsObject)) {
+				settings.commandPrefix = getString(JSONKeys.SETTINGS_COMMAND_PREFIX, jsObject);
+			} else {
+				needsWriting = true;
+			}
+
+			if (!isObjectNull(JSONKeys.SETTINGS_WELCOME_MESSAGE, jsObject)) {
+				settings.welcomeMessage = getString(JSONKeys.SETTINGS_WELCOME_MESSAGE, jsObject);
+			} else {
+				needsWriting = true;
+			}
 
 			// Longs
-			settings.welcomeChannel = Long.parseLong(object.get(JSONKeys.SETTINGS_WELCOME_CHANNEL).asString());
-			settings.welcomeRole = Long.parseLong(object.get(JSONKeys.SETTINGS_WELCOME_ROLE).asString());
-			settings.logChannel = Long.parseLong(object.get(JSONKeys.SETTINGS_LOG_CHANNEL).asString());
-			settings.muteRole = Long.parseLong(object.get(JSONKeys.SETTINGS_MUTE_ROLE).asString());
-			settings.muteChannel = Long.parseLong(object.get(JSONKeys.SETTINGS_MUTE_CHANNEL).asString());
-			settings.musicchannel = Long.parseLong(object.get(JSONKeys.SETTINGS_MUSIC_CHANNEL).asString());
-			settings.permission0 = Long.parseLong(object.get(JSONKeys.SETTINGS_ROLE_PERMISSION0).asString());
-			settings.permission1 = Long.parseLong(object.get(JSONKeys.SETTINGS_ROLE_PERMISSION1).asString());
-			settings.permission2 = Long.parseLong(object.get(JSONKeys.SETTINGS_ROLE_PERMISSION2).asString());
+			if (!isObjectNull(JSONKeys.SETTINGS_WELCOME_CHANNEL, jsObject)) {
+				settings.welcomeChannel = getLong(JSONKeys.SETTINGS_WELCOME_CHANNEL, jsObject);
+			} else {
+				needsWriting = true;
+			}
+
+			if (!isObjectNull(JSONKeys.SETTINGS_WELCOME_ROLE, jsObject)) {
+				settings.welcomeRole = getLong(JSONKeys.SETTINGS_WELCOME_ROLE, jsObject);
+			} else {
+				needsWriting = true;
+			}
+
+			if (!isObjectNull(JSONKeys.SETTINGS_LOG_CHANNEL, jsObject)) {
+				settings.logChannel = getLong(JSONKeys.SETTINGS_LOG_CHANNEL, jsObject);
+			} else {
+				needsWriting = true;
+			}
+
+			if (!isObjectNull(JSONKeys.SETTINGS_MUTE_ROLE, jsObject)) {
+				settings.muteRole = getLong(JSONKeys.SETTINGS_MUTE_ROLE, jsObject);
+			} else {
+				needsWriting = true;
+			}
+
+			if (!isObjectNull(JSONKeys.SETTINGS_MUTE_CHANNEL, jsObject)) {
+				settings.muteChannel = getLong(JSONKeys.SETTINGS_MUTE_CHANNEL, jsObject);
+			} else {
+				needsWriting = true;
+			}
+
+			if (!isObjectNull(JSONKeys.SETTINGS_MUSIC_CHANNEL, jsObject)) {
+				settings.musicchannel = getLong(JSONKeys.SETTINGS_MUSIC_CHANNEL, jsObject);
+			} else {
+				needsWriting = true;
+			}
+
+			if (!isObjectNull(JSONKeys.SETTINGS_ROLE_PERMISSION0, jsObject)) {
+				settings.permission0 = getLong(JSONKeys.SETTINGS_ROLE_PERMISSION0, jsObject);
+			} else {
+				needsWriting = true;
+			}
+
+			if (!isObjectNull(JSONKeys.SETTINGS_ROLE_PERMISSION1, jsObject)) {
+				settings.permission1 = getLong(JSONKeys.SETTINGS_ROLE_PERMISSION1, jsObject);
+			} else {
+				needsWriting = true;
+			}
+
+			if (!isObjectNull(JSONKeys.SETTINGS_ROLE_PERMISSION2, jsObject)) {
+				settings.permission2 = getLong(JSONKeys.SETTINGS_ROLE_PERMISSION2, jsObject);
+			} else {
+				needsWriting = true;
+			}
 
 			reader.close();
+
+			// Restore any objects that were missing or had issues
+			if (needsWriting) {
+				GuildSettingsWriter.writeSettings(settings);
+			}
 			return settings;
 		} catch (ParseException | IOException e) {
 			System.err.println("Error retrieving guild or guild settings for guild: " + guildID);
 		}
 
 		return settings;
+	}
+
+	private static boolean isObjectNull(String key, JsonObject jsonObject) {
+		return jsonObject.get(key) == null;
+	}
+
+	private static boolean getBool(String key, JsonObject jsonObject) {
+		return Boolean.parseBoolean(jsonObject.get(key).asString());
+	}
+
+	private static long getLong(String key, JsonObject jsonObject) {
+		return Long.parseLong(jsonObject.get(key).asString());
+	}
+
+	private static String getString(String key, JsonObject jsonObject) {
+		return jsonObject.get(key).asString();
 	}
 
 	public static String getSettingsValue(long guildID, String key) {
